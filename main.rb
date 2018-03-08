b4f3f049f9f3de4302b816cd4c8d62695528a522
Luan Gonçalves Barbosa <luan.goncbs@gmail.com>
2018-03-08 16:29:00 -0300

    Adds DB Group functionality

--- main.rb
+++ main.rb
@@ -6,29 +6,35 @@ Bundler.require
 ######################################################################################
 
 @databases = {
-	teste1: PG::Connection.open(host: :localhost , dbname: :teste1 , user: :luan, password: "P@55W0rdPG"),
-	teste2: PG::Connection.open(host: :localhost , dbname: :teste2 , user: :luan, password: "P@55W0rdPG"),
-	teste3: PG::Connection.open(host: :localhost , dbname: :teste3 , user: :luan, password: "P@55W0rdPG")
+  teste1: [PG::Connection.open(host: :localhost , dbname: :teste1 , user: :luan, password: "P@55W0rdPG")],
+  teste2: [PG::Connection.open(host: :localhost , dbname: :teste2 , user: :luan, password: "P@55W0rdPG")],
+  APP_STAGING: [PG::Connection.open(host: :localhost , dbname: :teste3 , user: :luan, password: "P@55W0rdPG")]
 }
+
+@databases[:APP_STAGING] << @databases[:teste2][0]
 
 ######################################################################################
 
-def add_user(name, password, db, permissions = nil)
-	begin
-		res = db.exec_params("CREATE ROLE #{name} LOGIN #{permissions.join(" ") unless permissions.nil?} ENCRYPTED PASSWORD '#{password}'")
-		puts res.cmd_status
-	rescue Exception => e
-		puts e.message
-	end
+def add_user(name, password, db, options = nil)
+	db.each do |db| 
+    begin
+  		res = db.exec_params("CREATE ROLE #{name} LOGIN #{options.join(" ") unless options.nil?} ENCRYPTED PASSWORD '#{password}'")
+  		puts res.cmd_status
+  	rescue Exception => e
+  		puts e.message
+  	end
+  end
 end
 
 def remove_user(name, db)
-	begin
-		res = db.exec_params("DROP ROLE IF EXISTS #{name}")
-		puts res.cmd_status
-	rescue Exception => e
-		puts e.message
-	end
+  db.each do |db| 
+  	begin
+  		res = db.exec_params("DROP ROLE IF EXISTS #{name}")
+  		puts res.cmd_status
+  	rescue Exception => e
+  		puts e.message
+  	end
+  end
 end
 
 def list_users(db)
@@ -68,45 +74,51 @@ end
 ###ROLES###
 
 def assign_role_admin(user, db)
-	begin
-	db.exec_params("GRANT USAGE ON SCHEMA public TO #{user}")
-	db.exec_params("GRANT ALL ON SCHEMA public TO #{user}")
-	db.exec_params("GRANT ALL ON ALL TABLES IN SCHEMA public TO #{user}")
-	puts "$ #{user} is now an ADMIN User."
-	rescue Exception => e
-		puts e.message
-	end
+	db.each do |db| 
+    begin
+    	db.exec_params("GRANT USAGE ON SCHEMA public TO #{user}")
+    	db.exec_params("GRANT ALL ON SCHEMA public TO #{user}")
+    	db.exec_params("GRANT ALL ON ALL TABLES IN SCHEMA public TO #{user}")
+    	puts "$ #{user} is now an ADMIN User."
+  	rescue Exception => e
+  		puts e.message
+  	end
+  end
 end
 
 def assign_role_read_only(user, db)
-	begin
-	db.exec_params("GRANT USAGE ON SCHEMA public TO #{user}")
-	db.exec_params("REVOKE CREATE ON SCHEMA public FROM public, #{user}")
-	db.exec_params("REVOKE ALL ON ALL TABLES IN SCHEMA public FROM #{user}")
-	db.exec_params("GRANT SELECT ON ALL TABLES IN SCHEMA public TO #{user}")
-	puts "$ #{user} is now a READ_ONLY User."
-	rescue Exception => e
-		puts e.message
-	end
+	 db.each do |db| 
+    begin
+    	db.exec_params("GRANT USAGE ON SCHEMA public TO #{user}")
+    	db.exec_params("REVOKE CREATE ON SCHEMA public FROM public, #{user}")
+    	db.exec_params("REVOKE ALL ON ALL TABLES IN SCHEMA public FROM #{user}")
+    	db.exec_params("GRANT SELECT ON ALL TABLES IN SCHEMA public TO #{user}")
+    	puts "$ #{user} is now a READ_ONLY User."
+  	rescue Exception => e
+  		puts e.message
+  	end
+  end
 end
 
 def assign_role_read_write(user, db)
-	begin
-	db.exec_params("GRANT USAGE ON SCHEMA public TO #{user}")
-	db.exec_params("REVOKE CREATE ON SCHEMA public FROM public, #{user}")
-	db.exec_params("GRANT ALL ON ALL TABLES IN SCHEMA public TO #{user}")
-	puts "$ #{user} is now a READ_WRITE User."
-	rescue Exception => e
-		puts e.message
+	 db.each do |db| 
+    begin
+    	db.exec_params("GRANT USAGE ON SCHEMA public TO #{user}")
+    	db.exec_params("REVOKE CREATE ON SCHEMA public FROM public, #{user}")
+    	db.exec_params("GRANT ALL ON ALL TABLES IN SCHEMA public TO #{user}")
+    	puts "$ #{user} is now a READ_WRITE User."
+  	rescue Exception => e
+  		puts e.message
+    end
 	end
 end
 
 ######################################################################################
 
 cmd_text = "Available commands:
-  add: Adds User to specified database
-  rm: Removes User from specified database
-  aru: Assign Role to User: assigns one of the presetted roles to a user on a given database
+  add: Adds User to specified database or DB Group
+  rm: Removes User from specified database or DB Group
+  aru: Assign Role to User: assigns one of the presetted roles to a user on a given database or DB Group
   lu: Lists all Users from specified database and respective attributes
   ld: Lists all Databases currently managed by PUMI
   cmd: displays this message
